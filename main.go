8273ec5fba825d4efc9cd6db3d3c516d3f52dbfa
Daniel Diaz Lajara <danieldiazlajara@gmail.com>
2023-04-03 16:14:39 +0200

    build: :building_construction: changed to user docker-compose + dockerfile

--- main.go
+++ main.go
@@ -21,15 +21,7 @@ func init() {
 	connection.GatewayPeer = os.Getenv("PEER_GATEWAY")
 }
 
-/*
-	mspID        = "Org1MSP"
-	cryptoPath   = "../../test-network/organizations/peerOrganizations/org1.example.com"
-	certPath     = cryptoPath + "/users/User1@org1.example.com/msp/signcerts/cert.pem"
-	keyPath      = cryptoPath + "/users/User1@org1.example.com/msp/keystore/"
-	tlsCertPath  = cryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
-	peerEndpoint = "localhost:7051"
-	gatewayPeer  = "peer0.org1.example.com"
-*/
+// TODO CHECK ENVS
 
 func main() {
 	router := gin.Default()
